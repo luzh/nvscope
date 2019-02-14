cdfe2522d18bf0c9756b3c4fc6ec67909eac8b7b
luzh <luzh@eng.ucsd.edu>
2019-02-14 03:27:04 -0800

    cmake: add nvart_set_pass_properties()

--- cmake/NVArtUtils.cmake
+++ cmake/NVArtUtils.cmake
@@ -32,6 +32,17 @@ function(nvart_fatal MSGSTR)
   message(FATAL_ERROR "${BoldRed}[NVArt-Fatal]:${ColorReset} ${MSGSTR}")
 endfunction()
 
+function(nvart_set_pass_properties PASS_TARGET)
+    if(NOT ${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+      # On non-Debug builds cmake automatically defines NDEBUG. Explicitly
+      # undefine it to enable opt's -stats and -debug output. See more details
+      # in: llvm/lib/cmake/llvm/HandleLLVMOptions.cmake
+      target_compile_options(${PASS_TARGET} PRIVATE -UNDEBUG)
+    endif()
+
+    target_include_directories(${PASS_TARGET} PRIVATE ${LLVM_INCLUDE_DIRS})
+endfunction()
+
 function(nvart_set_default_properties)
   nvart_print("Setting default properties for ${ARGV}")
   set_property(
