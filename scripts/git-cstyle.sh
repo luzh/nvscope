0563650c17277bfd89b518128c13f8b1221d9df9
luzh <luzh@eng.ucsd.edu>
2019-02-13 22:36:02 -0800

    scripts: add git-cstyle.sh

---
+++ scripts/git-cstyle.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CLANG_FORMAT=$PWD/llvm/bin/clang-format
+PAGER="less -FrX"
+
+if [ ! -f $CLANG_FORMAT ]; then
+  echo "Error: Invalid CLANG_FORMAT '$CLANG_FORMAT'"
+  exit 1
+fi
+
+MODIFIED_FILES=`git diff --name-only -- *.{c,cc,cpp,h,hpp}`
+
+show_diff() {
+  for file in $MODIFIED_FILES; do
+    $CLANG_FORMAT -style=llvm $file | git --no-pager diff --color=always --no-index -- $file -
+  done
+}
+
+show_diff | $PAGER
