6dffaccd1c224d0c131213f682cc02d54e3a0eaa
luzh <luzh@eng.ucsd.edu>
2019-02-14 14:01:48 -0800

    lib: update condstore's print messages

--- lib/nvart/condstore.c
+++ lib/nvart/condstore.c
@@ -4,8 +4,8 @@ void condstore(uint64_t *ptr, uint64_t val) {
   srand(time(0));
   if (rand() & 1) {
     *ptr = val;
-    printf("performed store %zu to %p\n", val, (void *)ptr);
+    printf("Performing store %zu to %p\n", val, (void *)ptr);
   } else {
-    printf("skipped store %zu to %p\n", val, (void *)ptr);
+    printf("Skipping store %zu to %p\n", val, (void *)ptr);
   }
 }
