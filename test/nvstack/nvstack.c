6e8d865678af2d41ec564b684b68de5558a8fa4d
luzh <luzh@eng.ucsd.edu>
2019-02-13 17:28:31 -0800

    test: add nvstack basic implementation

---
+++ test/nvstack/nvstack.c
@@ -0,0 +1,34 @@
+#include "common.h"
+
+#define BUFSIZE 100
+
+void push(uint64_t *top, uint64_t value) {
+  *top = value;
+  printwarn("Push value 0x%lx into the stack!\n", value);
+  _mm_sfence();
+}
+
+int main(int argc, char **argv) {
+  char *buf = malloc(BUFSIZE);
+
+  memset(buf, 0xAC, BUFSIZE);
+  _mm_clflush(buf);
+  _mm_sfence();
+
+  unsigned idx = 0;
+  if (argc > 1) {
+    idx = atoi(argv[1]);
+    printf("User-specified index is %u\n", idx);
+  }
+
+  idx = idx + 10; // to be transformed by the NVArt pass
+  printf("Transformed index is (idx + 10) %u\n", idx);
+
+  uint64_t *bufptr = (uint64_t *)(buf + idx);
+
+  push(bufptr, 0x35);
+
+  free(buf);
+
+  return 0;
+}
