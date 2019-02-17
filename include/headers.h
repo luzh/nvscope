8e4c5da8919964429cbcbd7534dbc04cce7f9844
luzh <luzh@eng.ucsd.edu>
2019-02-17 15:44:14 -0800

    include: add headers.h and pprint.h

---
+++ include/headers.h
@@ -0,0 +1,30 @@
+#ifndef _HEADERS_H_
+#define _HEADERS_H_
+
+/* a list of headers to resolve most dependencies in this repository */
+
+#include <argp.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <immintrin.h>
+#include <limits.h>
+#include <pthread.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/prctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#endif
