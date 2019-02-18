45bd546d12bea1e7983f61bf0bcfc61bb3e1d595
luzh <luzh@eng.ucsd.edu>
2019-02-17 17:36:44 -0800

    test: update print functions in nvstack

--- test/nvstack/nvstack.c
+++ test/nvstack/nvstack.c
@@ -1,10 +1,11 @@
-#include "common.h"
+#include "headers.h"
+#include "pprint.h"
 
 #define BUFSIZE 100
 
 void push(uint64_t *top, uint64_t value) {
   *top = value;
-  printwarn("Push value 0x%lx into the stack!\n", value);
+  PPWORK("Push value 0x%lx into the stack!", value);
   _mm_sfence();
 }
 
@@ -18,11 +19,11 @@ int main(int argc, char **argv) {
   unsigned idx = 0;
   if (argc > 1) {
     idx = atoi(argv[1]);
-    printf("User-specified index is %u\n", idx);
+    PPWORK("User-specified index is %u", idx);
   }
 
   idx = idx + 10;  // to be transformed by the NVArt pass
-  printf("Transformed index is (idx + 10) %u\n", idx);
+  PPWORK("Transformed index is (idx + 10) %u", idx);
 
   uint64_t *bufptr = (uint64_t *)(buf + idx);
 
