380cbf461cfb4d53f725001a74e07b31088867eb
luzh <luzh@eng.ucsd.edu>
2019-02-17 16:28:50 -0800

    scripts: let clang-format modify files

--- scripts/git-cstyle.sh
+++ scripts/git-cstyle.sh
@@ -2,22 +2,44 @@
 
 CLANG_FORMAT=$PWD/llvm/bin/clang-format
 PAGER="less -FrX"
+STYLE=google
 
 if [ ! -f $CLANG_FORMAT ]; then
-  echo "Error: Invalid CLANG_FORMAT '$CLANG_FORMAT'"
-  exit 1
+  echo "Error: Invalid CLANG_FORMAT '$CLANG_FORMAT'" && exit 1
 fi
 
-# Check all files in the source tree.
-FILES=`git ls-files -- *.{c,cc,cpp,h,hpp}`
-
-# Check modified files, both unstaged and staged.
-# FILES=`git diff --name-only HEAD -- *.{c,cc,cpp,h,hpp}`
-
-show_diff() {
-  for file in $FILES; do
-    $CLANG_FORMAT -style=google $file | git --no-pager diff --color=always --no-index -- $file -
-  done
+diffone() {
+  $CLANG_FORMAT -style=$STYLE $1 | git --no-pager diff --color=always --no-index -- $1 -
 }
 
-show_diff | $PAGER
+diffall() {
+  for file in $1; do diffone $file; done
+}
+
+if [ -z $1 ]; then
+  # Check all files in the source tree.
+  diffall "`git ls-files -- *.{c,cc,cpp,h,hpp}`" | $PAGER
+  # Check modified files, both unstaged and staged.
+  # diffall "`git diff --name-only HEAD -- *.{c,cc,cpp,h,hpp}`" | $PAGER
+else
+  if [ -d $1 ]; then
+    files=`git ls-files -- $1/*.{c,cc,cpp,h,hpp}`
+  elif [ -f $1 ]; then
+    files=$1
+  else
+    echo "Error: $1 does not exist!" && exit 1
+  fi
+
+  if [ -z $2 ]; then # only check the spedified files
+    diffall "$files" | $PAGER
+  elif [ $2 == "-I" ]; then # modify file in-place
+    for file in $files; do
+      if ! diffone $file > /dev/null; then
+        $CLANG_FORMAT -style=$STYLE -i $file
+        echo "Applied clang format to $file."
+      fi
+    done
+  else
+    echo "Error: unknown option '$2'" && exit 1
+  fi
+fi
