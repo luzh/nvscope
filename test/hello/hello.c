cad4327430af0901173db0a1a594b3270805405e
luzh <luzh@eng.ucsd.edu>
2019-03-20 15:08:53 -0700

    test: add test cases to hello

--- test/hello/hello.c
+++ test/hello/hello.c
@@ -11,8 +11,14 @@ int main(int argc, char** argv) {
   if (argc > 1) a = atoi(argv[1]);
   if (argc > 2) b = atoi(argv[2]);
 
+  // This addition should be changed to multiplication by the bin2mul pass.
   int res = a + b;
   printf("%d + %d = %d\n", a, b, res);
+
+  if (res != a * b) {
+    printf("%d * %d != %d\n", a, b, a * b);
+    return 1;
+  }
 
   return 0;
 }
