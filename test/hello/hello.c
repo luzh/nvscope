e8f740e16621a75cbf29c5d2882fe0879e435362
luzh <luzh@eng.ucsd.edu>
2019-03-19 23:19:04 -0700

    test: fix cstyle in hello

--- test/hello/hello.c
+++ test/hello/hello.c
@@ -8,10 +8,8 @@ int main(int argc, char** argv) {
 
   int a = 2, b = 3;
 
-  if (argc > 1)
-    a = atoi(argv[1]);
-  if (argc > 2)
-    b = atoi(argv[2]);
+  if (argc > 1) a = atoi(argv[1]);
+  if (argc > 2) b = atoi(argv[2]);
 
   int res = a + b;
   printf("%d + %d = %d\n", a, b, res);
