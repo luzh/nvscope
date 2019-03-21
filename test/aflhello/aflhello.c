86edbb0953a2b5ee2b16b2d63574e08a67436a92
luzh <luzh@eng.ucsd.edu>
2019-03-21 00:43:39 -0700

    test: add ctests for aflhello

--- test/aflhello/aflhello.c
+++ test/aflhello/aflhello.c
@@ -1,13 +1,10 @@
 #include "headers.h"
 
-#define BUFSIZE 100
-
 int main() {
   char key[4] = {0, 0, 0, 0};
 
   printf("Key: ");
   int chars = scanf("%c%c%c", &key[0], &key[1], &key[2]);
-  char *buf = malloc(BUFSIZE);
 
   printf("Got %d chars, key = %s\n", chars, key);
   if (key[0] == 'a') {
@@ -16,16 +13,12 @@ int main() {
       printf("Into path b\n");
       if (key[2] == 'c') {
         printf("Into path c\n");
-        memset(buf, 0xAC, BUFSIZE);
-        _mm_sfence();
         abort();  // simulating a bug causing a crash
       }
     }
   }
 
-  free(buf);
-
-  printf("Normal Exit\n");
+  printf("No bug found\n");
 
   return 0;
 }
