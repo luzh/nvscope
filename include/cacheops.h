9f48b313f4fa7beb9a675f2ab199ca71372c494f
luzh <luzh@eng.ucsd.edu>
2019-03-21 22:52:56 -0700

    Revert "include: implement cacheops according to libpmem"
    This reverts commit c0e8dd47c21a01a9e99f9ed81c1f01adb76b3133.

--- include/cacheops.h
+++ include/cacheops.h
@@ -12,6 +12,8 @@
  * NVScope can still identify real _mm_clflushopt() or _mm_clwb() calls if they
  * are supported on a target machine.
  *
+ * Do not use the faked operations for performance evaluation.
+ *
  * Most machines should support _mm_sfence() and _mm_clflush() so they are
  * simply wrapped.
  */
@@ -27,17 +29,16 @@ void clflush(void const *ptr) { _mm_clflush(ptr); }
  *   tail call void @llvm.x86.clflushopt(i8* %0)
  * is emulated by
  *   tail call void @clflushopt(i8* %0)
- *
- * The asm implementations are from pmdk/src/libpmem/x86_64/flush.h but they may
- * not be accurate for performance evaluation.
  */
 
 void __attribute__((optnone, noinline)) clflushopt(void const *ptr) {
-  asm volatile(".byte 0x66; clflush %0" : "+m"(*(volatile char *)(ptr)));
+  unsigned char loopcnt = *((char *)ptr);
+  while (loopcnt > 0) --loopcnt;
 }
 
 void __attribute__((optnone, noinline)) clwb(void const *ptr) {
-  asm volatile(".byte 0x66; xsaveopt %0" : "+m"(*(volatile char *)(ptr)));
+  unsigned char loopcnt = *((char *)ptr);
+  while (loopcnt > 0) --loopcnt;
 }
 
 #endif
